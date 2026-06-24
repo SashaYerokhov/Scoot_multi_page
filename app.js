98fd3f6e1e826aba7c73ece14f53f6c50db17d08
SashaYerokhov <Ghost0737@gmail.com>
2026-06-24 19:11:20 +0300

    Created about desctop-page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const media = window.matchMedia("(width < 450px)");
 
 function updateNavbar(event) {
   const isMobile = event.matches;
-  console.log(isMobile);
+  // console.log(isMobile);
   if (isMobile) {
     navbar.setAttribute("inert", "");
   } else {
@@ -31,3 +31,38 @@ openButton.addEventListener("click", openSidebar);
 closeButton.addEventListener("click", closeSidebar);
 
 updateNavbar(media);
+
+// Accordion
+function initAccordion() {
+  const root = document.querySelector(".faqs");
+  if (!root) return;
+
+  const items = document.querySelectorAll(".accordion__item");
+  // console.log(items);
+  const triggers = root.querySelectorAll(".accordion__trigger");
+  // console.log(triggers);
+
+  items[0].classList.add("active");
+  // console.log(items[0]);
+  triggers[0].setAttribute("aria-expanded", "true");
+
+  items.forEach((item) => {
+    const trigger = item.querySelector(".accordion__trigger");
+    // console.log(trigger);
+    trigger.addEventListener("click", () => {
+      const isActive = item.classList.contains("active");
+      // console.log(isActive);
+      items.forEach((itm) => {
+        // console.log(itm);
+        itm.classList.remove("active");
+        itm.querySelector(".accordion__trigger");
+        itm.setAttribute("aria-expanded", "false");
+      });
+      if (!isActive) {
+        item.classList.add("active");
+        trigger.setAttribute("aria-expanded", "true");
+      }
+    });
+  });
+}
+initAccordion();
